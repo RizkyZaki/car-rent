6f8b4d5420767c7377f9b3ad572f3b01cce32abe
RizkyZaki <ifsry416@gmail.com>
2024-05-07 07:03:40 +0700

    feat: Clear

--- resources/views/client/pages/detail/category.blade.php
+++ resources/views/client/pages/detail/category.blade.php
@@ -1,4 +1,7 @@
 @extends('client.layout.main')
+@php
+  $benefits = json_decode(appSetting()->benefit, true);
+@endphp
 @section('content-client')
   <section id="blog-1" class="bg--white-300 py-100 blog-section division">
     <div class="container">
@@ -8,16 +11,20 @@
       <div class="row justify-content-center">
         <div class="col-md-10 col-lg-9">
           <div class="section-title mb-70">
+
             <!-- Title -->
             <h2 class="s-50 w-700">{{ $cat->name }}</h2>
+
           </div>
         </div>
       </div>
 
 
       <div class="row">
+
+
         <!-- BLOG POST #1 -->
-        @foreach ($cat->blog as $item)
+        @foreach ($cat->posts as $item)
           <div class="col-md-6 col-lg-4">
             <div id="bp-1-1" class="blog-post wow fadeInUp">
 
@@ -31,26 +38,43 @@
               <div class="blog-post-txt">
 
                 <!-- Post Tag -->
-                <span class="post-tag color--pink-400">{{ $item->category->name }}</span>
 
                 <!-- Post Link -->
                 <h6 class="s-20 w-700">
-                  <a href="{{ url('blog/' . $item->slug) }}">{{ $item->title }}</a>
+                  {{-- <a href="{{ url('post/' . $item->slug) }}">{{ $item->title }}</a> --}}
+                  <a href="javascript:void(0);">{{ $item->title }}</a>
+
                 </h6>
+                <ul>
+                  @foreach ($item->rent as $item)
+                    <li>{{ $item->rent }} : {{ formatIDR($item->fee) }}</li>
+                  @endforeach
+                </ul>
                 <!-- Post Meta -->
                 <div class="blog-post-meta mt-20">
                   <ul class="post-meta-list ico-10">
 
                     <li>
-                      <p class="p-sm">{{ timesInd($item->created_at) }}</p>
+                      <a href="javascript:void(0);" class="btn btn--theme hover--theme"><span>&#9733;</span>
+                        5 </a>
+                    </li>
+                    <li>
+                      <a href="https://wa.me/{{ appSetting()->contact_phone }}"
+                        class="btn btn--theme hover--theme">Booking Now</a>
                     </li>
                   </ul>
                 </div>
+                <div class="row">
+                  @foreach ($benefits as $benefit)
+                    <div class="col-md-6">&#10003; {{ $benefit }}</div>
+                  @endforeach
+                </div>
 
-              </div> <!-- END BLOG POST TEXT -->
+              </div>
 
-            </div>
-          </div> <!-- END BLOG POST #1 -->
+            </div> <!-- END BLOG POST TEXT -->
+
+          </div>
         @endforeach
 
       </div> <!-- End row -->
