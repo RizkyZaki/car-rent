b5fbde59239f107f8fb2e062947995b9bdaf8a69
RizkyZaki <ifsry416@gmail.com>
2024-05-07 10:58:04 +0700

    ;

--- resources/views/client/pages/home.blade.php
+++ resources/views/client/pages/home.blade.php
@@ -165,20 +165,14 @@
 
                 <!-- Icon -->
                 <div class="fbox-ico-wrap">
-                  <div class="fbox-ico ico-50">
-                    <div class="shape-ico color--theme">
+                  <img src="{{ asset('storage/assets/image/' . $item->icon) }}" width="50" alt="">
 
-                      <img src="{{ asset('storage/assets/image/' . $item->icon) }}" width="50" alt="">
-
-                      <!-- Shape -->
-                      <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                          d="M69.8,-23C76.3,-2.7,57.6,25.4,32.9,42.8C8.1,60.3,-22.7,67,-39.1,54.8C-55.5,42.7,-57.5,11.7,-48.6,-11.9C-39.7,-35.5,-19.8,-51.7,5.9,-53.6C31.7,-55.6,63.3,-43.2,69.8,-23Z"
-                          transform="translate(100 100)" />
-                      </svg>
-
-                    </div>
-                  </div>
+                  <!-- Shape -->
+                  <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M69.8,-23C76.3,-2.7,57.6,25.4,32.9,42.8C8.1,60.3,-22.7,67,-39.1,54.8C-55.5,42.7,-57.5,11.7,-48.6,-11.9C-39.7,-35.5,-19.8,-51.7,5.9,-53.6C31.7,-55.6,63.3,-43.2,69.8,-23Z"
+                      transform="translate(100 100)" />
+                  </svg>
                 </div> <!-- End Icon -->
 
                 <!-- Text -->
